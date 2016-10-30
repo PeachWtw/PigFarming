c3aaf5b8e12be853e44dee8e92ebadc124c518ea
liuyawin <717200899@qq.com>
2016-10-30 23:00:49 +0800

    刘亚赢：修改前台K线图

--- pig/views.py
+++ pig/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-import simplejson as json
 # Create your views here.
 from django.http import HttpResponse
 from pig.models import  Test_Pig
