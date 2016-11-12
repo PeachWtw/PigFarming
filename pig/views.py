0f434ec5c392ab21820915b840b0fe8f0098b5a1
PeachWtw <799154579@qq.com>
2016-11-12 12:02:33 +0800

    加入了articletype的办法

--- pig/views.py
+++ pig/views.py
@@ -44,23 +44,28 @@ def func_handle_database(db_obj):
 
 
 #处理文章列表的函数，准备把处理数据库数据的模块函数合并到这个函数
-def func_handle_artList(db_obj, page):
+def func_handle_artList(db_obj, page, art_type):
     try:
-        if page < 1:
-            raise PageLessThanOneError("页面小于1 page=%d" % page)
+        #获取元组总数
+        temp_list = db_obj.objects.filter(bi_id__gte=1,type=art_type)
+        num = map(str, temp_list)
+        page_total=int(math.ceil(len(num) / 10.0))
         index_low = (page - 1) * 10
         index_high = page * 10
-        if page == 1:
-            temp_list = db_obj.objects.filter(bi_id__lte=index_high)
-        else:
-            temp_list = temp_list = db_obj.objects.filter(bi_id__lte=index_high).filter(bi_id__gte=index_low)
+        filter_list=temp_list.all()[index_low:index_high]
+        # if page < 1:
+        #     raise PageLessThanOneError("页面小于1 page=%d" % page)
+        # if num<=10 | page == 1:#如果文章总数小于10或者page等于1
+        #     index_high =10
+        #     temp_list = db_obj.objects.filter(bi_id__lte=index_high,type=art_type)
+        # else:
+        #     index_low = (page - 1) * 10
+        #     index_high = page * 10
+        #     temp_list = temp_list = db_obj.objects.filter(bi_id__gte=index_low,type=art_type).filter(bi_id__lte=index_high,type=art_type)
         L = []
-        for iter in temp_list:
+        for iter in filter_list:
             L.append(iter.res_dict())
-        #获取元组总数
-        temp_list = db_obj.objects.filter(bi_id__gte=1)
-        num = map(str, temp_list)
-        return L, int(math.ceil(len(num) / 10.0))
+        return L, page_total
     finally:
         pass
 
@@ -70,11 +75,28 @@ def func_getArticle(request):
     L = func_handle_database(BreedImprovement)
     return HttpResponse(json.dumps(L))
 
-
-#功能：获取文章列表
+#需求6,7,9,10
+    #功能：获取文章列表:
+     #生产管理，动保防疫模块:
+         #猪场管理 pigFarmManagement
+         #繁育管理 breedManagement
+         #饲养管理 feedManagement
+         #日常管理 dailyManagement
 def func_getArtList(request):
     page = request.GET['page']
-    L, cnt = func_handle_artList(BreedImprovement, int(page))
+    #article_type=request.GET['articleType']
+    article_type='pigFarmManagement'
+    if article_type=='pigFarmManagement':
+         L, cnt = func_handle_artList(BreedImprovement, int(page),article_type)
+    elif article_type=='breedManagement':
+         L, cnt = func_handle_artList(BreedImprovement, int(page),article_type)
+    elif article_type=='feedManagement':
+         L, cnt = func_handle_artList(BreedImprovement, int(page),article_type)
+    elif article_type=='dailyManagement':
+         L, cnt = func_handle_artList(BreedImprovement, int(page),article_type)
+    else:
+        L,cnt=None,None
+    # L, cnt = func_handle_artList(BreedImprovement, int(page))
     d = dict(allList=L, page=cnt)
     s = json.dumps(d)
     return HttpResponse(json.dumps(d))
